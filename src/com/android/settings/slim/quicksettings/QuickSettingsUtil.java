d5cb9d9fceb4f6e950f87785ed8a27ff0b6f6ae9
Evisceration <eviscerationls@gmail.com>
2014-03-15 14:13:40 -0500

    Add On-The-Go Toggle [2/2]
    Change-Id: Ia77e806e3b7fc4ab9956b3ffe890c830030788a0
    
    Conflicts:
    	res/values/ose_strings.xml
    	src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
    
    Conflicts:
    	res/values/ose_strings.xml
    
    Conflicts:
    	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

--- src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -58,6 +58,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.liquid.QSConstants.TILE_FCHARGE;
+import static com.android.internal.util.liquid.QSConstants.TILE_ONTHEGO;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -179,6 +180,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_FCHARGE, R.string.title_tile_fcharge,
                 "com.android.systemui:drawable/ic_qs_fcharge_off"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_ONTHEGO, R.string.title_tile_onthego,
+                "com.android.systemui:drawable/ic_qs_onthego"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
