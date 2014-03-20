terciocarvalho/android_packages_apps_Settings

Add option to Restart SystemUI temasek spare parts -> General Settings

src/com/android/settings/temasek/GeneralSettings.java
@@ -1,0 +1,53 @@
+package com.android.settings.temasek;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.util.Helpers;
+
+public class GeneralSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String RESTART_SYSTEMUI = "restart_systemui";
+
+    private Preference mRestartSystemUI;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.general_settings);
+
+        mRestartSystemUI = findPreference(RESTART_SYSTEMUI);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mRestartSystemUI) {
+            Helpers.restartSystemUI();
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}

SeniorLimpio/packages_apps_Settings

Conflicts:
	res/values/cr_strings.xml
Change-Id: I86a892ea4f629e470a9a98972d07266c96218f5c

src/com/android/settings/limpio/GeneralSettings.java
@@ -1,0 +1,53 @@
+package com.android.settings.limpio;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.util.Helpers;
+
+public class GeneralSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String RESTART_SYSTEMUI = "restart_systemui";
+
+    private Preference mRestartSystemUI;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.general_settings);
+
+        mRestartSystemUI = findPreference(RESTART_SYSTEMUI);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mRestartSystemUI) {
+            Helpers.restartSystemUI();
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}