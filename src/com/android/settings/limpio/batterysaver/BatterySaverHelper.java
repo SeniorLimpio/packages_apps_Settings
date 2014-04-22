2f976666051d49858f8cdfc6ac7658af99ccfeee
SeniorLimpio <seniorlimpio1@gmail.com>
2014-04-23 07:26:27 +1200

    Fix fc in battery saver settings

--- src/com/android/settings/limpio/batterysaver/BatterySaverHelper.java
+++ src/com/android/settings/limpio/batterysaver/BatterySaverHelper.java
@@ -36,7 +36,7 @@ public class BatterySaverHelper {
     private final static String TAG = "BatterySaverHelper";
 
     private static final String SCHEDULE_BATTERY_SAVER =
-            "com.android.settings.liquid.batterysaver.SCHEDULE_BATTERY_SAVER";
+            "com.android.settings.limpio.batterysaver.SCHEDULE_BATTERY_SAVER";
 
     private static final int FULL_DAY = 1440; // 1440 minutes in a day
 
