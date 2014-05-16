fafb4dd411523b1cf89103bbde01059654aa5ede
SeniorLimpio <seniorlimpio1@gmail.com>
2014-05-16 17:25:16 +1200

    LockScreen: allow long-press actions from hardware keys (2/2) Allow following actions with button long-presses on lockscreen: - toggle flashlight (if available) - play/pause/next/previous track selection - toggle silent mode
    Allow customizing of back, menu, and home buttons. Search
    button events are completely ignored on the lockscreen.
    
    Auto-detect hardware keys, only show appropriate menu
    options based on the actual hardware keys available on the
    device (or completely hide if there are no hardware keys).
    
    Based largely on Danny Baumann's work from gingerbread.
    
    Ported from CM10
    Change-Id: I36ea5eb7e93d68de872536dc6d7e93f99ba1621f
    
    Keyguard: Fix for home wake/unlock
    sometimes device is waking and unlocking during the same keypress.
    Change-Id: I72ea7611d39aac0cd03dfd5aa7036aeef7600140
    
    Use sticky broadcast to convey torch state (1/2).
    It doesn't really make sense to store the (non-persistent) state of the
    torch in a (persistent) setting. As the torch is sending a broadcast
    anyway, make that broadcast sticky and use it to convey the state.
    
    Also unify the torch constants (actions, extras, activity names) at a
    common place.
    Change-Id: If845bef27cc79990794aa748dce18cfea3bac1f5
    
    Fix Home/Menu/Back keys' unlock
    Change-Id: Ibd3842b6ad8e037a2a9f8eb581e7c55900017b0f
    
    LockScreen: allow hardware key longpress to put device to sleep
    Change-Id: I54260e21c462bcabd9b6383e22f44d644630b4d3
    
    settings: Deprecate has_led_flash, move to framework.
     * We need this to configure default tiles in SystemUI so move it.
     * Leave the old one in place so we don't kill every device.
    Change-Id: I883f9595f74ab52c8c8152e0105400aaefff4afe
    
    LockScreen: allow hardware key longpress to put device to sleep
    
    Original commit by jt1134

--- src/com/android/settings/limpio/LockscreenInterface.java
+++ src/com/android/settings/limpio/LockscreenInterface.java
@@ -110,7 +110,8 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     };
 
     public boolean hasButtons() {
-        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+        return (getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys) > 0);
     }
 
     @Override
