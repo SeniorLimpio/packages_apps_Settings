7c595937878bc16ec50cc94a58519529d15d6e34
Schubi <schubi@erlangen.ccc.de>
2014-05-31 13:10:30 +0200

    Slim Recents: Option to show topmost task [2/2]
    Change-Id: I46cd6d1141ffb6893c94b10a7f0d9d9f94b92ec1
    
    Conflicts:
    	res/values/cr_strings.xml
    	res/xml/recents_panel_settings.xml
    	src/com/android/settings/accessibility/AccessibilitySettings.java
    	src/com/android/settings/ldroid/RecentPanelSettings.java

--- src/com/android/settings/ldroid/RecentPanelSettings.java
+++ src/com/android/settings/ldroid/RecentPanelSettings.java
@@ -57,6 +57,7 @@ public class RecentPanelSettings extends SettingsPreferenceFragment
     private static final String RAM_BAR_COLOR_APP_MEM = "ram_bar_color_app_mem";
     private static final String RAM_BAR_COLOR_CACHE_MEM = "ram_bar_color_cache_mem";
     private static final String RAM_BAR_COLOR_TOTAL_MEM = "ram_bar_color_total_mem";
+    private static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
     private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
     private static final String RECENT_PANEL_SCALE = "recent_panel_scale";
     private static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
@@ -75,6 +76,7 @@ public class RecentPanelSettings extends SettingsPreferenceFragment
     private ColorPickerPreference mRamBarAppMemColor;
     private ColorPickerPreference mRamBarCacheMemColor;
     private ColorPickerPreference mRamBarTotalMemColor;
+    private CheckBoxPreference mRecentsShowTopmost;
     private CheckBoxPreference mRecentPanelLeftyMode;
     private ListPreference mRecentPanelScale;
     private ListPreference mRecentPanelExpandedMode;
@@ -139,6 +141,11 @@ public class RecentPanelSettings extends SettingsPreferenceFragment
         mRamBarTotalMemColor.setSummary(hexColor);
         mRamBarTotalMemColor.setNewPreviewColor(intColor);
 
+        boolean enableRecentsShowTopmost = Settings.System.getInt(getContentResolver(),
+                                      Settings.System.RECENT_PANEL_SHOW_TOPMOST, 0) == 1;
+        mRecentsShowTopmost = (CheckBoxPreference) findPreference(RECENT_PANEL_SHOW_TOPMOST);
+        mRecentsShowTopmost.setChecked(enableRecentsShowTopmost);
+        mRecentsShowTopmost.setOnPreferenceChangeListener(this);
         mRecentPanelLeftyMode = (CheckBoxPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
         mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
         mRecentPanelScale = (ListPreference) findPreference(RECENT_PANEL_SCALE);
@@ -242,6 +249,11 @@ public class RecentPanelSettings extends SettingsPreferenceFragment
             int value = Integer.parseInt((String) newValue);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.RECENT_PANEL_SCALE_FACTOR, value);
+            return true;
+        } else if (preference == mRecentsShowTopmost) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.RECENT_PANEL_SHOW_TOPMOST,
+                    ((Boolean) newValue) ? 1 : 0);
             return true;
         } else if (preference == mRecentPanelLeftyMode) {
             Settings.System.putInt(getContentResolver(),
