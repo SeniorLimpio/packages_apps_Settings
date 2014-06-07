ChameleonOS/android_packages_apps_Settings

Identicons: Add identicons to ChaOS Lab [2/3]
When enabled, new contacts will be assigned a unique
5x5 identicon as the contact photo instead of the boring
default one.

PS2:
Modify the selection used when updating a contact photo.
This seems to reduce the number of unchanged contacts after
the service is done running.

PS3:
Use the new GithubIdenticon class for now.

PS4:
Fix the creation service so it now correctly updates contacts
without photos by assigning an identicon.  Also added a removal
service which will go through and look for photos that have
been tagged as an identicon and remove it.

PS5:
Switch to JPG format and batch removal of identicons.

PS6:
Add identicons styles

PS7:
Add dot matrix style

PS8:
Clean up for release and add proper ChaOS Lab annotations.

Change-Id: Ie7f7a287b3b69d1eb498acd89c62b3b34ed7a875

src/com/android/settings/chameleonos/labs/identicons/IdenticonUtils.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.chameleonos.labs.identicons;
+
+import com.android.internal.util.chaos.identicons.Identicon;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class IdenticonUtils {
+
+    private static final byte[] JPG_HEADER = new byte[] { (byte)0xFF, (byte)0xD8 };
+
+    public static boolean isIdenticon(byte[] data) {
+        if (data == null || !isJpgFormat(data))
+            return false;
+
+        byte[] tag = Arrays.copyOfRange(data, data.length - 18, data.length - 2);
+        String tagString;
+        try {
+            tagString = new String(tag, "US-ASCII");
+        } catch (UnsupportedEncodingException e) {
+            return false;
+        }
+
+        return Identicon.IDENTICON_MARKER.equals(tagString);
+    }
+
+    private static boolean isJpgFormat(byte[] data) {
+        if (data.length < JPG_HEADER.length)
+            return false;
+
+        for (int i = 0; i < JPG_HEADER.length; i++) {
+            if (data[i] != JPG_HEADER[i])
+                return false;
+        }
+
+        return true;
+    }
+}

src/com/android/settings/chameleonos/labs/identicons/IdenticonsSettings.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.chameleonos.labs.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class IdenticonsSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+    private static final String TAG = "IdenticonsSettings";
+
+    private static final String KEY_ENABLED = "identicons_enabled";
+    private static final String KEY_STYLE = "identicons_style";
+    private static final String KEY_CREATE = "identicons_create";
+    private static final String KEY_REMOVE = "identicons_remove";
+
+    private SwitchPreference mEnabledPref;
+    private ImageListPreference mStylePref;
+
+    private CharSequence mPreviousTitle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.identicons_prefs);
+
+        mEnabledPref = (SwitchPreference) findPreference(KEY_ENABLED);
+        mEnabledPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.IDENTICONS_ENABLED, 0) == 1));
+        mEnabledPref.setOnPreferenceChangeListener(this);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mStylePref = (ImageListPreference) prefSet.findPreference(KEY_STYLE);
+        mStylePref.setOnPreferenceChangeListener(this);
+        int style = Settings.System.getInt(getContentResolver(),
+                Settings.System.IDENTICONS_STYLE, 0);
+        mStylePref.setValue(String.valueOf(style));
+        updateStyleSummary(style);
+
+        Preference startServicePref = findPreference(KEY_CREATE);
+        startServicePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                getActivity().startService(new Intent(getActivity(), IdenticonCreationService.class));
+                return true;
+            }
+        });
+
+        startServicePref = findPreference(KEY_REMOVE);
+        startServicePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                getActivity().startService(new Intent(getActivity(), IdenticonRemovalService.class));
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final ActionBar bar = getActivity().getActionBar();
+        mPreviousTitle = bar.getTitle();
+        bar.setTitle(R.string.identicons_title);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().getActionBar().setTitle(mPreviousTitle);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mEnabledPref) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.IDENTICONS_ENABLED,
+                    ((Boolean) newValue).booleanValue() ? 1 : 0);
+            return true;
+        } else if (preference == mStylePref) {
+            int style = Integer.valueOf((String) newValue);
+            updateStyleSummary(style);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateStyleSummary(int value) {
+        mStylePref.setSummary(mStylePref.getEntries()[mStylePref.findIndexOfValue("" + value)]);
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.IDENTICONS_STYLE, value);
+    }
+}

src/com/android/settings/chameleonos/labs/identicons/ImageListPreference.java
@@ -1,0 +1,132 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.chameleonos.labs.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.preference.ListPreference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+import com.android.settings.R;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class ImageListPreference extends ListPreference {
+    private int[] mResourceIds = null;
+    private int mRadioDrawableId;
+
+    /**
+     * Constructor of the ImageListPreference. Initializes the custom images.
+     * @param context application context.
+     * @param attrs custom xml attributes.
+     */
+    public ImageListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,
+                R.styleable.ImageListPreference);
+
+        String[] imageNames = context.getResources().getStringArray(
+                typedArray.getResourceId(typedArray.getIndexCount()-1, -1));
+
+        mResourceIds = new int[imageNames.length];
+
+        for (int i=0;i<imageNames.length;i++) {
+            String imageName = imageNames[i].substring(
+                    imageNames[i].lastIndexOf('/') + 1,
+                    imageNames[i].lastIndexOf('.'));
+
+            mResourceIds[i] = context.getResources().getIdentifier(imageName,
+                    "drawable", context.getPackageName());
+        }
+
+        typedArray.recycle();
+
+        // in order to use the holo themed radio button we must get the id
+        // from the system resources and then get the drawable for that id
+        // Because it uses reflection twice, we'll store this value for future use.
+        mRadioDrawableId = Resources.getSystem().getIdentifier("btn_radio_holo_dark", "drawable", "android");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void onPrepareDialogBuilder(Builder builder) {
+        int index = findIndexOfValue(getSharedPreferences().getString(
+                getKey(), "1"));
+
+        ListAdapter listAdapter = new ImageArrayAdapter(getContext(),
+                R.layout.image_list_item, getEntries(), mResourceIds, index);
+
+        // Order matters.
+        builder.setAdapter(listAdapter, this);
+        super.onPrepareDialogBuilder(builder);
+    }
+
+    public class ImageArrayAdapter extends ArrayAdapter<CharSequence> {
+        private int index = 0;
+        private int[] resourceIds = null;
+
+        /**
+         * ImageArrayAdapter constructor.
+         * @param context the context.
+         * @param textViewResourceId resource id of the text view.
+         * @param objects to be displayed.
+         * @param ids resource id of the images to be displayed.
+         * @param i index of the previous selected item.
+         */
+        public ImageArrayAdapter(Context context, int textViewResourceId,
+                                 CharSequence[] objects, int[] ids, int i) {
+            super(context, textViewResourceId, objects);
+
+            index = i;
+            resourceIds = ids;
+        }
+        /**
+         * {@inheritDoc}
+         */
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = ((Activity)getContext()).getLayoutInflater();
+            View row = inflater.inflate(R.layout.image_list_item, parent, false);
+
+            ImageView imageView = (ImageView)row.findViewById(R.id.image);
+            imageView.setImageResource(resourceIds[position]);
+
+            CheckedTextView checkedTextView = (CheckedTextView)row.findViewById(
+                    R.id.check);
+
+            checkedTextView.setText(getItem(position));
+            checkedTextView.setCheckMarkDrawable(Resources.getSystem().getDrawable(mRadioDrawableId));
+
+            if (position == index) {
+                checkedTextView.setChecked(true);
+            }
+
+            return row;
+        }
+    }
+}

SeniorLimpio/packages_apps_Settings

Conflicts:
	AndroidManifest.xml
	res/values/fusion_arrays.xml
	res/values/fusion_strings.xml
	res/xml/fusion_settings.xml
Signed-off-by: dragon <dragon7780@hotmail.nl>
	res/values/ldroid_attrs.xml
	res/values/sl_arrays.xml
	res/values/sl_strings.xml
	res/xml/limpio_settings.xml

src/com/android/settings/ldroid/labs/identicons/IdenticonUtils.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.ldroid.labs.identicons;
+
+import com.android.internal.util.ldroid.identicons.Identicon;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class IdenticonUtils {
+
+    private static final byte[] JPG_HEADER = new byte[] { (byte)0xFF, (byte)0xD8 };
+
+    public static boolean isIdenticon(byte[] data) {
+        if (data == null || !isJpgFormat(data))
+            return false;
+
+        byte[] tag = Arrays.copyOfRange(data, data.length - 18, data.length - 2);
+        String tagString;
+        try {
+            tagString = new String(tag, "US-ASCII");
+        } catch (UnsupportedEncodingException e) {
+            return false;
+        }
+
+        return Identicon.IDENTICON_MARKER.equals(tagString);
+    }
+
+    private static boolean isJpgFormat(byte[] data) {
+        if (data.length < JPG_HEADER.length)
+            return false;
+
+        for (int i = 0; i < JPG_HEADER.length; i++) {
+            if (data[i] != JPG_HEADER[i])
+                return false;
+        }
+
+        return true;
+    }
+}

src/com/android/settings/ldroid/labs/identicons/IdenticonsSettings.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.ldroid.labs.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class IdenticonsSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+    private static final String TAG = "IdenticonsSettings";
+
+    private static final String KEY_ENABLED = "identicons_enabled";
+    private static final String KEY_STYLE = "identicons_style";
+    private static final String KEY_CREATE = "identicons_create";
+    private static final String KEY_REMOVE = "identicons_remove";
+
+    private SwitchPreference mEnabledPref;
+    private ImageListPreference mStylePref;
+
+    private CharSequence mPreviousTitle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.identicons_prefs);
+
+        mEnabledPref = (SwitchPreference) findPreference(KEY_ENABLED);
+        mEnabledPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.IDENTICONS_ENABLED, 0) == 1));
+        mEnabledPref.setOnPreferenceChangeListener(this);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mStylePref = (ImageListPreference) prefSet.findPreference(KEY_STYLE);
+        mStylePref.setOnPreferenceChangeListener(this);
+        int style = Settings.System.getInt(getContentResolver(),
+                Settings.System.IDENTICONS_STYLE, 0);
+        mStylePref.setValue(String.valueOf(style));
+        updateStyleSummary(style);
+
+        Preference startServicePref = findPreference(KEY_CREATE);
+        startServicePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                getActivity().startService(new Intent(getActivity(), IdenticonCreationService.class));
+                return true;
+            }
+        });
+
+        startServicePref = findPreference(KEY_REMOVE);
+        startServicePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                getActivity().startService(new Intent(getActivity(), IdenticonRemovalService.class));
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final ActionBar bar = getActivity().getActionBar();
+        mPreviousTitle = bar.getTitle();
+        bar.setTitle(R.string.identicons_title);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().getActionBar().setTitle(mPreviousTitle);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mEnabledPref) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.IDENTICONS_ENABLED,
+                    ((Boolean) newValue).booleanValue() ? 1 : 0);
+            return true;
+        } else if (preference == mStylePref) {
+            int style = Integer.valueOf((String) newValue);
+            updateStyleSummary(style);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateStyleSummary(int value) {
+        mStylePref.setSummary(mStylePref.getEntries()[mStylePref.findIndexOfValue("" + value)]);
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.IDENTICONS_STYLE, value);
+    }
+}

src/com/android/settings/ldroid/labs/identicons/ImageListPreference.java
@@ -1,0 +1,132 @@
+/*
+ * Copyright (C) 2013 The ChameleonOS Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.ldroid.labs.identicons;
+
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.preference.ListPreference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+import com.android.settings.R;
+
+@ChaosLab(name="QuickStats", classification=Classification.NEW_CLASS)
+public class ImageListPreference extends ListPreference {
+    private int[] mResourceIds = null;
+    private int mRadioDrawableId;
+
+    /**
+     * Constructor of the ImageListPreference. Initializes the custom images.
+     * @param context application context.
+     * @param attrs custom xml attributes.
+     */
+    public ImageListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,
+                R.styleable.ImageListPreference);
+
+        String[] imageNames = context.getResources().getStringArray(
+                typedArray.getResourceId(typedArray.getIndexCount()-1, -1));
+
+        mResourceIds = new int[imageNames.length];
+
+        for (int i=0;i<imageNames.length;i++) {
+            String imageName = imageNames[i].substring(
+                    imageNames[i].lastIndexOf('/') + 1,
+                    imageNames[i].lastIndexOf('.'));
+
+            mResourceIds[i] = context.getResources().getIdentifier(imageName,
+                    "drawable", context.getPackageName());
+        }
+
+        typedArray.recycle();
+
+        // in order to use the holo themed radio button we must get the id
+        // from the system resources and then get the drawable for that id
+        // Because it uses reflection twice, we'll store this value for future use.
+        mRadioDrawableId = Resources.getSystem().getIdentifier("btn_radio_holo_dark", "drawable", "android");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void onPrepareDialogBuilder(Builder builder) {
+        int index = findIndexOfValue(getSharedPreferences().getString(
+                getKey(), "1"));
+
+        ListAdapter listAdapter = new ImageArrayAdapter(getContext(),
+                R.layout.image_list_item, getEntries(), mResourceIds, index);
+
+        // Order matters.
+        builder.setAdapter(listAdapter, this);
+        super.onPrepareDialogBuilder(builder);
+    }
+
+    public class ImageArrayAdapter extends ArrayAdapter<CharSequence> {
+        private int index = 0;
+        private int[] resourceIds = null;
+
+        /**
+         * ImageArrayAdapter constructor.
+         * @param context the context.
+         * @param textViewResourceId resource id of the text view.
+         * @param objects to be displayed.
+         * @param ids resource id of the images to be displayed.
+         * @param i index of the previous selected item.
+         */
+        public ImageArrayAdapter(Context context, int textViewResourceId,
+                                 CharSequence[] objects, int[] ids, int i) {
+            super(context, textViewResourceId, objects);
+
+            index = i;
+            resourceIds = ids;
+        }
+        /**
+         * {@inheritDoc}
+         */
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = ((Activity)getContext()).getLayoutInflater();
+            View row = inflater.inflate(R.layout.image_list_item, parent, false);
+
+            ImageView imageView = (ImageView)row.findViewById(R.id.image);
+            imageView.setImageResource(resourceIds[position]);
+
+            CheckedTextView checkedTextView = (CheckedTextView)row.findViewById(
+                    R.id.check);
+
+            checkedTextView.setText(getItem(position));
+            checkedTextView.setCheckMarkDrawable(Resources.getSystem().getDrawable(mRadioDrawableId));
+
+            if (position == index) {
+                checkedTextView.setChecked(true);
+            }
+
+            return row;
+        }
+    }
+}