burstlam/android_packages_apps_Settings

QuickSettings : Add BatterySaver Tile (2/2)

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -60,6 +60,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_PROFILE;
 import static com.android.internal.util.slim.QSConstants.TILE_CAMERA;
 import static com.android.internal.util.slim.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
+import static com.android.internal.util.slim.QSConstants.TILE_BATTERYSAVER;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -187,6 +188,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_ONTHEGO, R.string.title_tile_onthego,
                 "com.android.systemui:drawable/ic_qs_onthego"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_BATTERYSAVER, R.string.title_tile_batterysaver,
+                "com.android.systemui:drawable/ic_qs_battery_saver_on"));
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CONTACT, R.string.title_tile_contact,
                 "com.android.systemui:drawable/ic_qs_default_user"));

SeniorLimpio/packages_apps_Settings

Conflicts:
	res/values/liquid_strings.xml
	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -23,6 +23,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_AIRPLANE;
 import static com.android.internal.util.liquid.QSConstants.TILE_ALARM;
 import static com.android.internal.util.liquid.QSConstants.TILE_AUTOROTATE;
 import static com.android.internal.util.liquid.QSConstants.TILE_BATTERY;
+import static com.android.internal.util.liquid.QSConstants.TILE_BATTERYSAVER;
 import static com.android.internal.util.liquid.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.liquid.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.liquid.QSConstants.TILE_BUGREPORT;
@@ -182,6 +183,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_ONTHEGO, R.string.title_tile_onthego,
                 "com.android.systemui:drawable/ic_qs_onthego"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_BATTERYSAVER, R.string.title_tile_batterysaver,
+                "com.android.systemui:drawable/ic_qs_battery_saver_on"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {