LiquidSmooth-Old/android_packages_apps_Settings

Refactor settings resources and liquify imports

src/com/android/settings/liquid/quicksettings/QuickSettingsTilesStyle.java
@@ -1,0 +1,323 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.liquid.quicksettings;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.android.internal.util.liquid.DeviceUtils;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+import com.android.settings.widget.SeekBarPreference;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "QuickSettingsTilesStyle";
+
+    private static final String PREF_TILES_PER_ROW =
+            "tiles_per_row";
+    private static final String PREF_TILES_PER_ROW_DUPLICATE_LANDSCAPE =
+            "tiles_per_row_duplicate_landscape";
+    private static final String PREF_QUICK_TILES_BG_COLOR =
+            "quick_tiles_bg_color";
+    private static final String PREF_QUICK_TILES_BG_PRESSED_COLOR =
+            "quick_tiles_bg_pressed_color";
+    private static final String PREF_QUICK_TILES_ALPHA =
+            "quick_tiles_alpha";
+    private static final String PREF_QUICK_TILES_TEXT_COLOR =
+            "quick_tiles_text_color";
+    private static final String PREF_ADDITIONAL_OPTIONS =
+            "quicksettings_tiles_style_additional_options";
+
+    private static final int DEFAULT_QUICK_TILES_TEXT_COLOR = 0xffcccccc;
+
+    private static final int MENU_RESET = Menu.FIRST;
+
+    private static final int DLG_RESET = 0;
+
+    private ListPreference mTilesPerRow;
+    private CheckBoxPreference mDuplicateColumnsLandscape;
+    private ColorPickerPreference mQuickTilesBgColor;
+    private ColorPickerPreference mQuickTilesBgPressedColor;
+    private ColorPickerPreference mQuickTilesTextColor;
+    private SeekBarPreference mQsTileAlpha;
+
+    private boolean mCheckPreferences;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    private PreferenceScreen refreshSettings() {
+        mCheckPreferences = false;
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.quicksettings_tiles_style);
+
+        prefs = getPreferenceScreen();
+
+        PackageManager pm = getPackageManager();
+        Resources systemUiResources;
+        try {
+            systemUiResources = pm.getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            Log.e(TAG, "can't access systemui resources",e);
+            return null;
+        }
+
+        int intColor;
+        String hexColor;
+
+        mQuickTilesBgColor = (ColorPickerPreference) findPreference(PREF_QUICK_TILES_BG_COLOR);
+        mQuickTilesBgColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_COLOR, -2);
+        if (intColor == -2) {
+            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                    "com.android.systemui:color/qs_background_color", null, null));
+            mQuickTilesBgColor.setSummary(getResources().getString(R.string.default_string));
+        } else {
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mQuickTilesBgColor.setSummary(hexColor);
+        }
+        mQuickTilesBgColor.setNewPreviewColor(intColor);
+
+        mQuickTilesBgPressedColor =
+                (ColorPickerPreference) findPreference(PREF_QUICK_TILES_BG_PRESSED_COLOR);
+        mQuickTilesBgPressedColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_PRESSED_COLOR, -2);
+        if (intColor == -2) {
+            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                    "com.android.systemui:color/qs_background_pressed_color", null, null));
+            mQuickTilesBgPressedColor.setSummary(getResources().getString(R.string.default_string));
+        } else {
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mQuickTilesBgPressedColor.setSummary(hexColor);
+        }
+        mQuickTilesBgPressedColor.setNewPreviewColor(intColor);
+
+        mQuickTilesTextColor = (ColorPickerPreference) findPreference(PREF_QUICK_TILES_TEXT_COLOR);
+        mQuickTilesTextColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_TEXT_COLOR, -2);
+        if (intColor == -2) {
+            intColor = DEFAULT_QUICK_TILES_TEXT_COLOR;
+            mQuickTilesTextColor.setSummary(getResources().getString(R.string.default_string));
+        } else {
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mQuickTilesTextColor.setSummary(hexColor);
+        }
+        mQuickTilesTextColor.setNewPreviewColor(intColor);
+
+        float transparency;
+        try{
+            transparency = Settings.System.getFloat(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_ALPHA);
+        } catch (Exception e) {
+            transparency = 0;
+            Settings.System.putFloat(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_ALPHA, 0.0f);
+        }
+        if (mQsTileAlpha == null) {
+            mQsTileAlpha = (SeekBarPreference) findPreference(PREF_QUICK_TILES_ALPHA);
+            mQsTileAlpha.setInitValue((int) (transparency * 100));
+            mQsTileAlpha.setOnPreferenceChangeListener(this);
+        }
+
+        mTilesPerRow = (ListPreference) prefs.findPreference(PREF_TILES_PER_ROW);
+        int tilesPerRow = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.QUICK_TILES_PER_ROW, 3);
+        mTilesPerRow.setValue(String.valueOf(tilesPerRow));
+        mTilesPerRow.setSummary(mTilesPerRow.getEntry());
+        mTilesPerRow.setOnPreferenceChangeListener(this);
+
+        mDuplicateColumnsLandscape =
+            (CheckBoxPreference) findPreference(PREF_TILES_PER_ROW_DUPLICATE_LANDSCAPE);
+        mDuplicateColumnsLandscape.setChecked(Settings.System.getInt(
+                getActivity().getContentResolver(),
+                Settings.System.QUICK_TILES_PER_ROW_DUPLICATE_LANDSCAPE, 1) == 1);
+        mDuplicateColumnsLandscape.setOnPreferenceChangeListener(this);
+
+        PreferenceCategory additionalOptions =
+            (PreferenceCategory) findPreference(PREF_ADDITIONAL_OPTIONS);
+        if (!DeviceUtils.isPhone(getActivity())) {
+            additionalOptions.removePreference(
+                findPreference(PREF_TILES_PER_ROW_DUPLICATE_LANDSCAPE));
+        }
+
+        setHasOptionsMenu(true);
+        mCheckPreferences = true;
+        return prefs;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_settings_backup) // use the backup icon
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (!mCheckPreferences) {
+            return false;
+        }
+        if (preference == mTilesPerRow) {
+            int index = mTilesPerRow.findIndexOfValue((String) newValue);
+            int value = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_PER_ROW,
+                    value);
+            mTilesPerRow.setSummary(mTilesPerRow.getEntries()[index]);
+            return true;
+        } else if (preference == mDuplicateColumnsLandscape) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_PER_ROW_DUPLICATE_LANDSCAPE,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mQuickTilesBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mQuickTilesBgPressedColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_PRESSED_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mQuickTilesTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QUICK_TILES_TEXT_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mQsTileAlpha) {
+            float valNav = Float.parseFloat((String) newValue);
+            Settings.System.putFloat(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_ALPHA, valNav / 100);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        QuickSettingsTilesStyle getOwner() {
+            return (QuickSettingsTilesStyle) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.qs_style_reset_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getActivity().getContentResolver(),
+                                    Settings.System.QUICK_TILES_BG_COLOR, -2);
+                            Settings.System.putInt(getActivity().getContentResolver(),
+                                    Settings.System.QUICK_TILES_BG_PRESSED_COLOR, -2);
+                            Settings.System.putInt(getActivity().getContentResolver(),
+                                    Settings.System.QUICK_TILES_TEXT_COLOR, -2);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
+    }
+
+}

SeniorLimpio/packages_apps_Settings

Conflicts:
	res/values-de/slim_strings.xml
	res/values-es/slim_strings.xml
	res/values-ru/slim_strings.xml
	res/xml/security_settings_chooser.xml
	res/xml/slim_navigation_settings.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

src/com/android/settings/liquid/quicksettings/QuickSettingsTilesStyle.java
@@ -1,0 +1,323 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.liquid.quicksettings;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.android.internal.util.liquid.DeviceUtils;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "QuickSettingsTilesStyle";
+
+    private static final String PREF_TILES_PER_ROW =
+            "tiles_per_row";
+    private static final String PREF_TILES_PER_ROW_DUPLICATE_LANDSCAPE =
+            "tiles_per_row_duplicate_landscape";
+    private static final String PREF_QUICK_TILES_BG_COLOR =
+            "quick_tiles_bg_color";
+    private static final String PREF_QUICK_TILES_BG_PRESSED_COLOR =
+            "quick_tiles_bg_pressed_color";
+    private static final String PREF_QUICK_TILES_ALPHA =
+            "quick_tiles_alpha";
+    private static final String PREF_QUICK_TILES_TEXT_COLOR =
+            "quick_tiles_text_color";
+    private static final String PREF_ADDITIONAL_OPTIONS =
+            "quicksettings_tiles_style_additional_options";
+
+    private static final int DEFAULT_QUICK_TILES_TEXT_COLOR = 0xffcccccc;
+
+    private static final int MENU_RESET = Menu.FIRST;
+
+    private static final int DLG_RESET = 0;
+
+    private ListPreference mTilesPerRow;
+    private CheckBoxPreference mDuplicateColumnsLandscape;
+    private ColorPickerPreference mQuickTilesBgColor;
+    private ColorPickerPreference mQuickTilesBgPressedColor;
+    private ColorPickerPreference mQuickTilesTextColor;
+    private SlimSeekBarPreference mQsTileAlpha;
+
+    private boolean mCheckPreferences;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    private PreferenceScreen refreshSettings() {
+        mCheckPreferences = false;
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.quicksettings_tiles_style);
+
+        prefs = getPreferenceScreen();
+
+        PackageManager pm = getPackageManager();
+        Resources systemUiResources;
+        try {
+            systemUiResources = pm.getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            Log.e(TAG, "can't access systemui resources",e);
+            return null;
+        }
+
+        int intColor;
+        String hexColor;
+
+        mQuickTilesBgColor = (ColorPickerPreference) findPreference(PREF_QUICK_TILES_BG_COLOR);
+        mQuickTilesBgColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_COLOR, -2);
+        if (intColor == -2) {
+            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                    "com.android.systemui:color/qs_background_color", null, null));
+            mQuickTilesBgColor.setSummary(getResources().getString(R.string.default_string));
+        } else {
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mQuickTilesBgColor.setSummary(hexColor);
+        }
+        mQuickTilesBgColor.setNewPreviewColor(intColor);
+
+        mQuickTilesBgPressedColor =
+                (ColorPickerPreference) findPreference(PREF_QUICK_TILES_BG_PRESSED_COLOR);
+        mQuickTilesBgPressedColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_PRESSED_COLOR, -2);
+        if (intColor == -2) {
+            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                    "com.android.systemui:color/qs_background_pressed_color", null, null));
+            mQuickTilesBgPressedColor.setSummary(getResources().getString(R.string.default_string));
+        } else {
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mQuickTilesBgPressedColor.setSummary(hexColor);
+        }
+        mQuickTilesBgPressedColor.setNewPreviewColor(intColor);
+
+        mQuickTilesTextColor = (ColorPickerPreference) findPreference(PREF_QUICK_TILES_TEXT_COLOR);
+        mQuickTilesTextColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_TEXT_COLOR, -2);
+        if (intColor == -2) {
+            intColor = DEFAULT_QUICK_TILES_TEXT_COLOR;
+            mQuickTilesTextColor.setSummary(getResources().getString(R.string.default_string));
+        } else {
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mQuickTilesTextColor.setSummary(hexColor);
+        }
+        mQuickTilesTextColor.setNewPreviewColor(intColor);
+
+        float transparency;
+        try{
+            transparency = Settings.System.getFloat(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_ALPHA);
+        } catch (Exception e) {
+            transparency = 0;
+            Settings.System.putFloat(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_ALPHA, 0.0f);
+        }
+        if (mQsTileAlpha == null) {
+            mQsTileAlpha = (SlimSeekBarPreference) findPreference(PREF_QUICK_TILES_ALPHA);
+            mQsTileAlpha.setInitValue((int) (transparency * 100));
+            mQsTileAlpha.setOnPreferenceChangeListener(this);
+        }
+
+        mTilesPerRow = (ListPreference) prefs.findPreference(PREF_TILES_PER_ROW);
+        int tilesPerRow = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.QUICK_TILES_PER_ROW, 3);
+        mTilesPerRow.setValue(String.valueOf(tilesPerRow));
+        mTilesPerRow.setSummary(mTilesPerRow.getEntry());
+        mTilesPerRow.setOnPreferenceChangeListener(this);
+
+        mDuplicateColumnsLandscape =
+            (CheckBoxPreference) findPreference(PREF_TILES_PER_ROW_DUPLICATE_LANDSCAPE);
+        mDuplicateColumnsLandscape.setChecked(Settings.System.getInt(
+                getActivity().getContentResolver(),
+                Settings.System.QUICK_TILES_PER_ROW_DUPLICATE_LANDSCAPE, 1) == 1);
+        mDuplicateColumnsLandscape.setOnPreferenceChangeListener(this);
+
+        PreferenceCategory additionalOptions =
+            (PreferenceCategory) findPreference(PREF_ADDITIONAL_OPTIONS);
+        if (!DeviceUtils.isPhone(getActivity())) {
+            additionalOptions.removePreference(
+                findPreference(PREF_TILES_PER_ROW_DUPLICATE_LANDSCAPE));
+        }
+
+        setHasOptionsMenu(true);
+        mCheckPreferences = true;
+        return prefs;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_settings_backup) // use the backup icon
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (!mCheckPreferences) {
+            return false;
+        }
+        if (preference == mTilesPerRow) {
+            int index = mTilesPerRow.findIndexOfValue((String) newValue);
+            int value = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_PER_ROW,
+                    value);
+            mTilesPerRow.setSummary(mTilesPerRow.getEntries()[index]);
+            return true;
+        } else if (preference == mDuplicateColumnsLandscape) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.QUICK_TILES_PER_ROW_DUPLICATE_LANDSCAPE,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mQuickTilesBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mQuickTilesBgPressedColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_PRESSED_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mQuickTilesTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QUICK_TILES_TEXT_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mQsTileAlpha) {
+            float valNav = Float.parseFloat((String) newValue);
+            Settings.System.putFloat(getContentResolver(),
+                    Settings.System.QUICK_TILES_BG_ALPHA, valNav / 100);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        QuickSettingsTilesStyle getOwner() {
+            return (QuickSettingsTilesStyle) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.qs_style_reset_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getActivity().getContentResolver(),
+                                    Settings.System.QUICK_TILES_BG_COLOR, -2);
+                            Settings.System.putInt(getActivity().getContentResolver(),
+                                    Settings.System.QUICK_TILES_BG_PRESSED_COLOR, -2);
+                            Settings.System.putInt(getActivity().getContentResolver(),
+                                    Settings.System.QUICK_TILES_TEXT_COLOR, -2);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
+    }
+
+}