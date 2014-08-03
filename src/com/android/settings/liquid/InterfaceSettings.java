Validus-Pura/android_packages_apps_Settings

[2/2] settings: Change density on the fly
Change-Id: Ifd036c75f0423c334f8e25aaf6b3229833e779d7

src/com/android/settings/slim/InterfaceSettings.java
@@ -16,32 +16,255 @@
 
 package com.android.settings.slim;
 
+import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ComponentName;
 import android.content.Context;
-import android.content.res.Resources;
+import android.content.DialogInterface;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.provider.Settings;
-import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
-import android.preference.PreferenceCategory;
-import android.preference.Preference.OnPreferenceChangeListener;
+import android.text.Editable;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.Slog;
+import android.view.IWindowManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.lang.Thread;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class InterfaceSettings extends SettingsPreferenceFragment {
 
+    private static final String TAG = "InterfaceSettings";
+
+    private static final String KEY_LCD_DENSITY = "lcd_density";
+
+    private static final int DIALOG_CUSTOM_DENSITY = 101;
+
+    private static final String DENSITY_PROP = "persist.sys.lcd_density";
+
+    private static Preference mLcdDensity;
+
+    private static int mMaxDensity = DisplayMetrics.getDeviceDensity();
+    private static int mDefaultDensity = getSlimDefaultDensity();
+    private static int mMinDensity = getMinimumDensity();
+
+    private static Activity mActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mActivity = getActivity();
+
+        updateSettings();
+    }
+
+    private void updateSettings() {
+        setPreferenceScreen(null);
         addPreferencesFromResource(R.xml.slim_interface_settings);
+
+        mLcdDensity = (Preference) findPreference(KEY_LCD_DENSITY);
+        String current = SystemProperties.get(DENSITY_PROP,
+                SystemProperties.get("ro.sf.lcd_density"));
+        mLcdDensity.setSummary(getResources().getString(R.string.current_density) + current);
+        mLcdDensity.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                showDialogInner(DIALOG_CUSTOM_DENSITY);
+                return true;
+            }
+        });
+    }
+
+    private static int getMinimumDensity() {
+        int min = -1;
+        int[] densities = { 91, 121, 161, 241, 321, 481 };
+        for (int density : densities) {
+            if (density < mMaxDensity) {
+                min = density;
+            }
+        }
+        return min;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    private static void setDensity(int density) {
+        if (density < mMinDensity || density > mMaxDensity) {
+            return;
+        }
+        SystemProperties.set(DENSITY_PROP, Integer.toString(density));
+        final IWindowManager windowManagerService = IWindowManager.Stub.asInterface(
+                ServiceManager.getService(Context.WINDOW_SERVICE));
+        try {
+            windowManagerService.updateStatusBarNavBarHeight();
+        } catch (RemoteException e) {
+            Slog.w(TAG, "Failure communicating with window manager", e);
+        }
+        Configuration configuration = new Configuration();
+        configuration.setToDefaults();
+        configuration.densityDpi = density;
+        try {
+            ActivityManagerNative.getDefault().updateConfiguration(configuration);
+        } catch (RemoteException e) {
+            Slog.w(TAG, "Failure communicating with activity manager", e);
+        }
+        killRunningApps();
+    }
+
+    private static void killRunningApps() {
+        ActivityManager am = (ActivityManager) mActivity.getSystemService(
+                Context.ACTIVITY_SERVICE);
+        String defaultKeyboard = Settings.Secure.getStringForUser(mActivity.getContentResolver(),
+                Settings.Secure.DEFAULT_INPUT_METHOD, UserHandle.USER_CURRENT);
+        am.forceStopPackage(defaultKeyboard);
+        for (ActivityManager.RunningAppProcessInfo pid : am.getRunningAppProcesses()) {
+            am.killBackgroundProcesses(pid.processName);
+        }
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        InterfaceSettings getOwner() {
+            return (InterfaceSettings) getTargetFragment();
+        }
+
+        private void setTextDPI(TextView tv, String dpi) {
+            tv.setText(getResources().getString(R.string.new_density) + dpi);
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            LayoutInflater factory = LayoutInflater.from(getActivity());
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DIALOG_CUSTOM_DENSITY:
+                    final View dialogView = factory.inflate(
+                            R.layout.density_changer_dialog, null);
+                    final TextView currentDPI = (TextView)
+                            dialogView.findViewById(R.id.current_dpi);
+                    final TextView newDPI = (TextView) dialogView.findViewById(R.id.new_dpi);
+                    final SeekBar dpi = (SeekBar) dialogView.findViewById(R.id.dpi_edit);
+                    String current = SystemProperties.get(DENSITY_PROP,
+                            SystemProperties.get("ro.sf.lcd_density"));
+                    setTextDPI(newDPI, current);
+                    currentDPI.setText(getResources().getString(
+                            R.string.current_density) + current);
+                    dpi.setMax(mMaxDensity - mMinDensity);
+                    dpi.setProgress(Integer.parseInt(current) - mMinDensity);
+                    dpi.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                        @Override
+                        public void onProgressChanged(SeekBar seekBar,
+                                int progress, boolean fromUser) {
+                            int value = progress + mMinDensity;
+                            setTextDPI(newDPI, Integer.toString(value));
+                        }
+
+                        @Override
+                        public void onStartTrackingTouch(SeekBar seekBar) {
+                        }
+
+                        @Override
+                        public void onStopTrackingTouch(SeekBar seekBar) {
+                        }
+                    });
+                    ImageView setDefault = (ImageView) dialogView.findViewById(R.id.default_dpi);
+                    setDefault.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if (mDefaultDensity != -1) {
+                                dpi.setProgress(mDefaultDensity - mMinDensity);
+                                setTextDPI(newDPI, Integer.toString(mDefaultDensity));
+                            }
+                        }
+                    });
+                    return new AlertDialog.Builder(getActivity())
+                            .setTitle(getResources().getString(R.string.set_custom_density_title))
+                            .setView(dialogView)
+                            .setPositiveButton(getResources().getString(
+                                    R.string.set_custom_density_set),
+                                    new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dialog.dismiss();
+                                    setDensity(dpi.getProgress() + mMinDensity);
+                                }
+                            })
+                            .setNegativeButton(getResources().getString(R.string.cancel),
+                                    new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+        }
+    }
+
+    public static int getSlimDefaultDensity() {
+        Properties prop = new Properties();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("/system/build.prop");
+            prop.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+        return Integer.parseInt(prop.getProperty(DENSITY_PROP, "-1"));
     }
 }

SeniorLimpio/packages_apps_Settings

Conflicts:
	res/values/liquid_strings.xml
	src/com/android/settings/liquid/InterfaceSettings.java

src/com/android/settings/liquid/InterfaceSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 SlimRoms
+ * Copyright (C) 2014 The LiquidSmooth Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,32 +16,180 @@
 
 package com.android.settings.liquid;
 
-import android.app.ActivityManager;
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
-import android.content.res.Resources;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.text.Editable;
+import android.util.Slog;
+import android.view.IWindowManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class InterfaceSettings extends SettingsPreferenceFragment {
+import java.util.Arrays;
+import java.util.ArrayList;
 
+public class InterfaceSettings extends SettingsPreferenceFragment
+    implements OnPreferenceChangeListener {
+
+    private static final String TAG = "InterfaceSettings";
+
+    private static final String KEY_LCD_DENSITY = "lcd_density";
+    private static final String DENSITY_PROP = "persist.sys.lcd_density";
+
+    private static final int DIALOG_CUSTOM_DENSITY = 101;
+
+    private static ListPreference mLcdDensity;
+    private static Activity mActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.liquid_interface_settings);
+
+        mActivity = getActivity();
+
+        updateSettings();
+    }
+
+    private void updateSettings() {
+        mLcdDensity = (ListPreference) findPreference(KEY_LCD_DENSITY);
+        String current = SystemProperties.get(DENSITY_PROP,
+                SystemProperties.get("ro.sf.lcd_density"));
+        final ArrayList<String> array = new ArrayList<String>(
+                Arrays.asList(getResources().getStringArray(R.array.lcd_density_entries)));
+        if (array.contains(current)) {
+            mLcdDensity.setValue(current);
+        } else {
+            mLcdDensity.setValue("custom");
+        }
+        mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + current);
+        mLcdDensity.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+	
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+     }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLcdDensity) {
+            String density = (String) newValue;
+            if (SystemProperties.get(DENSITY_PROP) != density) {
+                if ((density).equals(getResources().getString(R.string.custom_density))) {
+                    showDialogInner(DIALOG_CUSTOM_DENSITY);
+                } else {
+                    setDensity(Integer.parseInt(density));
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private static void setDensity(int density) {
+        int max = mActivity.getResources().getInteger(R.integer.lcd_density_max);
+        int min = mActivity.getResources().getInteger(R.integer.lcd_density_min);
+        if (density < min || density > max) {
+            mLcdDensity.setSummary(mActivity.getResources().getString(
+                                            R.string.custom_density_summary_invalid));
+        }
+        SystemProperties.set(DENSITY_PROP, Integer.toString(density));
+        Configuration configuration = new Configuration();
+        configuration.setToDefaults();
+        configuration.densityDpi = density;
+        try {
+            ActivityManagerNative.getDefault().updateConfiguration(configuration);
+        } catch (RemoteException e) {
+            Slog.w(TAG, "Failure communicating with activity manager", e);
+        }
+        final IWindowManager windowManagerService = IWindowManager.Stub.asInterface(
+                ServiceManager.getService(Context.WINDOW_SERVICE));
+        try {
+            windowManagerService.updateStatusBarNavBarHeight();
+        } catch (RemoteException e) {
+            Slog.w(TAG, "Failure communicating with window manager", e);
+        }
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        InterfaceSettings getOwner() {
+            return (InterfaceSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            LayoutInflater factory = LayoutInflater.from(getActivity());
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DIALOG_CUSTOM_DENSITY:
+                    final View textEntryView = factory.inflate(
+                            R.layout.alert_dialog_text_entry, null);
+                    return new AlertDialog.Builder(getActivity())
+                            .setTitle(getResources().getString(R.string.set_custom_density_title))
+                            .setView(textEntryView)
+                            .setPositiveButton(getResources().getString(
+                                    R.string.set_custom_density_set),
+                                    new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    EditText dpi = (EditText)
+                                            textEntryView.findViewById(R.id.dpi_edit);
+                                    Editable text = dpi.getText();
+                                    dialog.dismiss();
+                                    setDensity(Integer.parseInt(text.toString()));
+                                }
+                            })
+                            .setNegativeButton(getResources().getString(R.string.cancel),
+                                    new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+        }
     }
 }